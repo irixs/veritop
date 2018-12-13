0896dabc4097a31f30b2be110dbfdd816527498f
Íris Soares <irissoares.silva@gmail.com>
2018-12-13 18:13:16 -0200

    projetop super sayajin 2.1

--- CPU.v
+++ CPU.v
@@ -1,13 +1,14 @@
-module CPU(etapa,tx,ty,tz,tula,entrada,barramento,acumulador,saida,clock,ulaout,estado);
+module CPU(etapa,tx,ty,tz,tula,entrada,barramento,acumulador,saida,clock,ulaout,estado,contador);
 	input wire clock;
-	input [4:0] entrada;
-	output[4:0] saida,acumulador;
+	output[4:0] entrada;
+	output[4:0] saida,acumulador,contador;
 	output[4:0] etapa,tx,ty,tz,tula,barramento,ulaout,estado;
 	
-	CONTROLE controle(etapa,tx,ty,tz,tula,clock,estado);
+	CONTROLE controle(etapa,tx,ty,tz,tula,clock,estado,contador);
 	ULA ula(barramento, acumulador, ulaout, tula);
 	REGISTRADORx Rx(entrada,barramento,tx,clock);
 	REGISTRADORy Ry(acumulador,ulaout,ty,clock);
 	REGISTRADORz Rz(saida,acumulador,tz,clock);
+	MEMORIA mem (tx,entrada,etapa,contador,clock);
 	
 endmodule
--- MEMORIA.v
+++ MEMORIA.v
@@ -1,32 +1,28 @@
-module MEMORIA(Tx,entrada,etapa,contador,clock);
-    input wire [1:0] Tx;
+module MEMORIA(tx,entrada,etapa,contador,clock);
+    input wire [4:0] tx;
     input wire clock;
-    output reg [4:0] entrada, etapa;
-    output integer contador;
-    parameter RESET = 2'b00;
-    parameter LOAD = 2'b01;
-    parameter HOLD = 2'b10;
-    parameter SHIFTL = 2'b11;
+    output reg [4:0] entrada,etapa,contador;
+    parameter LOAD = 4'd1;
  
     initial begin
-		entrada<= 4'd2;
+		entrada<= 4'd5;
 		etapa<= 4'd0;
-        contador <= 1'd0;
+        contador <= 4'd0;
     end
        
     always @(posedge clock) begin
-        if(Tx == LOAD) begin
+        if(tx == LOAD) begin
             if(contador==0)begin
-				entrada <= 4'd3;
-				contador<= 1'd1;
+				entrada <= 4'd2;
+				contador <= 4'd1;
             end
             if(contador==1)begin
-				entrada <= 4'd4;
-				contador<= 1'd2;
+				entrada <= 4'd3;
+				contador <= 4'd2;
 			end
-			if (contador==2)begin
-				entrada <= 4'd2;
-				contador <= 1'd0;
+			if(contador==2)begin
+				entrada <= 4'd5;
+				contador <= 4'd0;
 			end
         end
     end
